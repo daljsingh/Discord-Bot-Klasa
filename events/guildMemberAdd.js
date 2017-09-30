4453c064a5f565da8e2b97e607e56b868c86b745
Skillz <gankstarsleague@gmail.com>
2017-09-30 11:56:25 -0400

    edited for taO

---
+++ events/guildMemberAdd.js
@@ -0,0 +1,6 @@
+exports.run = (client, member) => {
+  if (member.guild.id === '67200685216641024') {
+    return member.send('**__Welcome To The Vainglory Server__**\n\nPlease type `$save IGN Region` and replace *IGN* with your *In Game Name* and replace *Region* with your Vainglory Region(na, sa, eu, ea, sea, or cn).')
+    // \n\nThen go to the Vainglory server in the *#bot-commands-here* channel type `$vainglorygiveaway IGN` to be entered into the <:ice:291837305998540810> giveaway lottery!'
+  }
+}
