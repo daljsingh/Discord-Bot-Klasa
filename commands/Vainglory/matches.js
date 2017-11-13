20b65f3d5c87e69c60f04984b9642d23801a1c66
Skillz <gankstarsleague@gmail.com>
2017-11-13 12:31:56 -0500

    fix for the matches error

--- commands/Vainglory/matches.js
+++ commands/Vainglory/matches.js
@@ -72,7 +72,7 @@ module.exports = class extends Command {
     const vainglory = new Vainglory(config.vgKey, options)
     await vainglory.region(region).matches.collection(options).then(async (matches) => {
       if (matches.errors) {
-        console.log(matches)
+        console.log(`Error: ${matches.messages} at ${matches.debug.url}`)
         return msg.reply('Please check the IGN and Region and try again. The API returned an error saying incorrect IGN or region.')
       }
       let winLose
